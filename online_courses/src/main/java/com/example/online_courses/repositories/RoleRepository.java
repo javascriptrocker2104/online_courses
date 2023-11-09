d4b44089446c190bfc178bb785524effa84d6f78
yarssset <76969319+yarssset@users.noreply.github.com>
2023-11-09 21:06:00 +0300

    Delete online_courses directory

--- online_courses/src/main/java/com/example/online_courses/repositories/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.online_courses.repositories;
-
-import com.example.online_courses.models.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-
-}
