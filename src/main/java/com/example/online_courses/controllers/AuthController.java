3cc749643b55af4c4511423b4e6e2c3de8e7d5dc
yarssset <76969319+yarssset@users.noreply.github.com>
2023-12-13 13:52:46 +0300

    Версия 13.12 капча
    Добавлена капча

--- src/main/java/com/example/online_courses/controllers/AuthController.java
+++ src/main/java/com/example/online_courses/controllers/AuthController.java
@@ -5,8 +5,11 @@ import com.example.online_courses.exceptions.UserAlreadyExistException;
 import com.example.online_courses.models.Content;
 import com.example.online_courses.models.User;
 import com.example.online_courses.repositories.UserRepository;
+import com.example.online_courses.service.RecaptchaService;
 import com.example.online_courses.service.UserDetailsServiceImpl;
 import com.example.online_courses.service.interfaces.UserService;
+
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +17,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
+import org.apache.commons.lang3.StringUtils;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 
@@ -26,11 +31,32 @@ public class AuthController {
     @Autowired
     private UserService userService;
 
+    private RecaptchaService recaptchaService;
+
     @GetMapping("/login")
     public String getLoginPage() {
         return "login";
     }
 
+    @PostMapping("/login")
+    public ResponseEntity<?> signup(@Valid User user,
+                                    @RequestParam(name="g-recaptcha-response") String recaptchaResponse,
+                                    HttpServletRequest request
+    ){
+
+        String ip = request.getRemoteAddr();
+        String captchaVerifyMessage =
+                recaptchaService.verifyRecaptcha(ip, recaptchaResponse);
+
+        if ( StringUtils.isNotEmpty(captchaVerifyMessage)) {
+            Map<String, Object> response = new HashMap<>();
+            response.put("message", captchaVerifyMessage);
+            return ResponseEntity.badRequest()
+                    .body(response);
+        }
+        userRepository.save(user);
+        return ResponseEntity.ok().build();
+    }
 
     @GetMapping("/registration")
     public String register(final Model model){
@@ -54,7 +80,7 @@ public class AuthController {
         return "redirect:/login";
     }
 
-    @PostMapping
+
 
     @RequestMapping(value = "/User/{id}/hide", method = RequestMethod.PUT)
     public ResponseEntity<String> hideUser(@PathVariable("id") UUID id) {
