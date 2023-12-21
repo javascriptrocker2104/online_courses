9d3ff54bb376537d6ca23586325d4b2e1f46d2ae
yarssset <76969319+yarssset@users.noreply.github.com>
2023-12-21 23:14:51 +0300

    Delete src/main/src/main directory

--- src/main/src/main/java/com/example/online_courses/repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.online_courses.repositories;
-
-import com.example.online_courses.models.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface RoleRepository extends JpaRepository<Role, UUID> {
-    Role findByName(String name);
-}
